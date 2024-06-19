26d3789f5b2158a8b0e7aef189cc79f331e24893
MihirKotecha <mihir.k1@ahduni.edu.in>
2024-06-19 12:56:58 +0530

    Formatting

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Gallery = () => {
   return (
     <div class="bg-[#f3eeea] h-screen w-full">
-    <div>
-    <h1 class="font-bold text-2xl pt-10 pl-20">Gallery</h1>
-    <p class="font-light text-base text-center pt-10 pl-10 w-60">Glance through some of our most memorable events</p>
-     
+      <div>
+        <h1 class="font-bold text-2xl pt-10 pl-20">Gallery</h1>
+        <p class="font-light text-base text-center pt-10 pl-10 w-60">
+          Glance through some of our most memorable events
+        </p>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
