3e781f98c70ea744ebca0ab1c0de036b5c079e53
MihirKotecha <mihir.k1@ahduni.edu.in>
2024-06-19 12:52:19 +0530

    Changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+      
       <Header />
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          
     </div>
   );
 };
