7a7d3160f579cb6628b34c036d2b3ccc83e82e92
MihirKotecha <mihir.k1@ahduni.edu.in>
2024-06-19 12:42:30 +0530

    Tailwind Cofing & Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import acm_logo from "../assets/acm_logo.jpg"
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex justify-between items-center w-full bg-[#f3eeea] h-20'>
+        <div className=' w-30 h-auto flex flex-wrap items-center'>
+            <img src={acm_logo} className='p-2 m-2 w-16 h-16'></img>
+            <h1 className='font-bold'>Ahmedabad University</h1>
+        </div>
+        <div>
+            
+        </div>
+    </div>
   )
 }
 
